078fefa3e475775c374ea6e8bd21fe3a8d649085
Ben Burgos <ben.a.burgos@gmail.com>
2022-03-19 17:51:33 -0700

    init commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// API Key 625581a608103e2740b59d94973c7176
+
+const $input = $("input")
+const $button = $("button")
+const $aside = $("aside")
+
+$button.on("click", () => {
+    $("span").empty();
+    const searchTerm = $input.val()
+    $.ajax(`https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=625581a608103e2740b59d94973c7176&units=imperial`)
+    .then((getWeather) => {
+        $("span.location").append(`${getWeather.name}`)
+        $("span.temperature-actual").append(`${getWeather.main.temp}°`)
+        $("span.temperature-feel").append(`${getWeather.main.feels_like}°`)
+        $("span.precipitation").append(`${getWeather.weather[0].description}`)
+    })
+})
+
